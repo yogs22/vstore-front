a89a404375b1279fdb1bb1943c1fdc35418d9e3c
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2022-07-15 06:38:32 +0700

    Refactor transactions member page

---
+++ components/organisms/TransactionContent/button-tab.tsx
@@ -0,0 +1,20 @@
+import cx from 'classnames';
+
+interface ButtonTabProps {
+  title: string;
+  active: boolean;
+}
+
+export default function ButtonTab(props: ButtonTabProps) {
+  const { title, active } = props;
+  const btnClass = cx({
+    'btn btn-status rounded-pill text-sm me-3': true,
+    'btn-active': active,
+  });
+
+  return (
+    <a datafilter="*" href="#" className={btnClass}>
+      {title}
+    </a>
+  );
+}
