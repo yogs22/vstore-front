d02f64d0847d5a54a25f5387145b610c900de237
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2022-07-15 07:00:30 +0700

    Refactor transaction detail page

---
+++ components/organisms/TransactionDetailContent/index.tsx
@@ -0,0 +1,72 @@
+import Row from './row.tsx';
+
+export default function TransactionDetailContent() {
+  return (
+    <main className="main-wrapper">
+      <div className="ps-lg-0">
+        <h2 className="text-4xl fw-bold color-palette-1 mb-30">Details #GG001</h2>
+        <div className="details">
+          <div className="main-content main-content-card overflow-auto">
+            <section className="checkout mx-auto">
+              <div className="d-flex flex-row  align-items-center justify-content-between mb-30">
+                <div className="game-checkout d-flex flex-row align-items-center">
+                  <div className="pe-4">
+                    <div className="cropped">
+                      <img
+                        src="/img/Thumbnail-3.png"
+                        width="200"
+                        height="130"
+                        className="img-fluid"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <p className="fw-bold text-xl color-palette-1 mb-10">
+                      Mobile Legends:
+                      <br />
+                      {' '}
+                      The New
+                      Battle 2021
+                    </p>
+                    <p className="color-palette-2 m-0">Category: Mobile</p>
+                  </div>
+                </div>
+                <div>
+                  <p className="fw-medium text-center label pending m-0 rounded-pill">Pending</p>
+                </div>
+              </div>
+              <hr />
+              <div className="purchase pt-30">
+                <h2 className="fw-bold text-xl color-palette-1 mb-20">Purchase Details</h2>
+                <Row label="Your Game ID" value="30012389" />
+                <Row label="Order ID" value="#GG-001" />
+                <Row label="Item" value="250 Diamond" />
+                <Row label="Price" value="2000000" />
+                <Row label="Tax (10%)" value="200000" />
+                <Row label="Total" value="1800000" className="color-palette-4" />
+              </div>
+              <div className="payment pt-10 pb-10">
+                <h2 className="fw-bold text-xl color-palette-1 mb-20">Payment Informations</h2>
+                <Row label="Your Account Name" value="Leonhart" />
+                <Row label="Type" value="Worldwide Transfer" />
+                <Row label="Bank Name" value="Mandiri" />
+                <Row label="Bank Account Name" value="Yogi Prasetyawan" />
+                <Row label="Bank Account Number" value="300012313123" />
+              </div>
+              <div className="d-md-block d-flex flex-column w-100">
+                <a
+                  className="btn btn-whatsapp rounded-pill fw-medium text-white border-0 text-lg"
+                  href="#"
+                  role="button"
+                >
+                  WhatsApp ke Admin
+                </a>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
