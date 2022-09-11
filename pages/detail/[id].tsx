853721159779d0be1adb2662d4c6e697a193eb5d
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2022-09-11 11:33:20 +0700

    Bug fix localstorage checkout

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,36 +1,21 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect } from 'react';
 import TopUpForm from '../../components/organisms/TopUpForm/index.tsx';
 import TopUpItem from '../../components/organisms/TopUpItem/index.tsx';
 import Navbar from '../../components/organisms/Navbar/index.tsx';
 import Footer from '../../components/organisms/Footer/index.tsx';
-import { getDetailVoucher } from '../../services/player.ts';
+import { getDetailVoucher, getFeaturedGame } from '../../services/player.ts';
+import { GameItemTypes, NominalsTypes, PaymentTypes } from '../../services/data-types/index.ts';
 
-export default function Detail() {
-  const { query, isReady } = useRouter();
-  const [dataItem, setDataItem] = useState({
-    name: '',
-    thumbnail: '',
-    category: {
-      name: '',
-    },
-  });
-  const [nominals, setNominals] = useState([]);
-  const [payments, setPayments] = useState([]);
+interface DetailProps {
+  dataItem: GameItemTypes,
+  nominals: NominalsTypes[],
+  payments: PaymentTypes[]
+}
 
-  const getVoucherDetailAPI = useCallback(async (id) => {
-    const data = await getDetailVoucher(id);
-    setDataItem(data.data);
-    localStorage.setItem('data-item', JSON.stringify(data.data));
-    setNominals(data.data.nominals);
-    setPayments(data.payment);
-  }, []);
-
+export default function Detail({ dataItem, nominals, payments } : DetailProps) {
   useEffect(() => {
-    if (isReady) {
-      getVoucherDetailAPI(query.id);
-    }
-  }, [isReady]);
+    localStorage.setItem('data-item', JSON.stringify(dataItem));
+  });
   return (
     <>
       <Navbar />
@@ -55,4 +40,35 @@ export default function Detail() {
       <Footer />
     </>
   );
+}
+
+export async function getStaticPaths() {
+  const data = await getFeaturedGame();
+  const paths = data.map((item: GameItemTypes) => ({
+    params: {
+      id: item._id,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+interface GetStaticProps {
+  params: {
+    id: string;
+  }
+}
+
+export async function getStaticProps({ params }: GetStaticProps) {
+  const { id } = params;
+  const data = await getDetailVoucher(id);
+  return {
+    props: {
+      dataItem: data.data,
+      nominals: data.data.nominals,
+      payments: data.payment,
+    },
+  };
 }
